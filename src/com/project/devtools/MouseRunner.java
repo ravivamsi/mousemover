d101e28a4563e4df571f49ba1f9622fc57a1740a
Vamsi Ravi <ivamsiravi@gmail.com>
2017-04-24 20:36:24 -0400

    Working model of the Mouse Mover

--- src/com/project/devtools/MouseRunner.java
+++ src/com/project/devtools/MouseRunner.java
@@ -3,6 +3,8 @@
  */
 package com.project.devtools;
 
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
 import java.util.Random;
 
@@ -14,12 +16,19 @@ import com.project.devtools.constants.ConstantValues;
  */
 public class MouseRunner {
 
+	static Double xPosition = MouseInfo.getPointerInfo().getLocation().getX();
+	static Double yPosition = MouseInfo.getPointerInfo().getLocation().getY();
+	static int x = Integer.valueOf(xPosition.intValue())+1;
+	static int y = Integer.valueOf(yPosition.intValue())+1;
 	public static void main(String... args) throws Exception {
         Robot robot = new Robot();
-        Random random = new Random();
         while (true) {
-            robot.mouseMove(random.nextInt(ConstantValues.MAX_X), random.nextInt(ConstantValues.MAX_Y));
+            robot.mouseMove(x,y);
+            
             Thread.sleep(ConstantValues.FIVE_SECONDS);
+            
+            robot.mouseMove(x-1, y-1);
+            
         }
     }
 }
